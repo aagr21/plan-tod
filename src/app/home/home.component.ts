f984ff6df85ac27aa1823686d13f7f8f1229e458
Arnol Guevara <arnolpacman@gmail.com>
2024-09-25 15:19:01 -0400

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MainContentComponent } from './main-content/main-content.component';
 import { DirectoriesService } from '@services/directories.service';
 import { Directory } from '@models/interfaces';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { Subscription } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -13,17 +15,32 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   directoriesService = inject(DirectoriesService);
   currentDirectory?: Directory;
   allDirectories: Directory[] = [];
+  private subscription!: Subscription;
+  breakpointObserver = inject(BreakpointObserver);
+  isSmallScreen = false;
 
   ngOnInit(): void {
+    this.subscription = this.breakpointObserver
+      .observe([Breakpoints.HandsetPortrait])
+      .subscribe({
+        next: (result) => {
+          this.isSmallScreen = result.matches;
+        },
+      });
+
     this.directoriesService.findAll().subscribe({
       next: (response) => {
         this.allDirectories = response;
         this.directoriesService.setAllDirectories(this.allDirectories);
       },
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
